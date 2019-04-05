d18834aae96776712d54316f6b23789f9b4b5ded
Thomas Gervolino <thomas_gervolino@hotmail.com>
2019-04-05 15:52:31 +0100

    Added Remove pet function

--- pet_shop.rb
+++ pet_shop.rb
@@ -17,7 +17,6 @@ def pets_sold(pet_shop)
   return pet_shop[:admin][:pets_sold]
 end
 
-
 def increase_pets_sold(pet_shop, number_of_sales)
   pet_shop[:admin][:pets_sold] += number_of_sales
 end
@@ -43,4 +42,12 @@ def find_pet_by_name(pet_shop, pet_name)
     end
   end
   return nil
+end
+
+def remove_pet_by_name(pet_shop, pet_name)
+  for pet in pet_shop[:pets]
+    if pet[:name] == pet_name
+      pet_shop[:pets].delete(pet)
+    end
+  end
 end
