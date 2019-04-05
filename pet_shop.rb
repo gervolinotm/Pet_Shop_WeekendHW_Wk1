3982a5e105b3d402f9190991b79b6d11c73c4bc9
Thomas Gervolino <thomas_gervolino@hotmail.com>
2019-04-05 14:32:08 +0100

    Added pet_name search

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,18 +1,22 @@
 def pet_shop_name(pet_shop)
   return pet_shop[:name]
 end
+# Accessing the name location of @pet_shop
 
 def total_cash(pet_shop)
   return pet_shop[:admin][:total_cash]
 end
+# Acessing the location of total_cash in @pet_shop
 
 def add_or_remove_cash(pet_shop, amount)
   pet_shop[:admin][:total_cash] += amount
 end
+# Acessing total_cash and giving it the method (+=) where it would make changes to the total_cash based on the amount(-10 or 10).
 
 def pets_sold(pet_shop)
   return pet_shop[:admin][:pets_sold]
 end
+
 
 def increase_pets_sold(pet_shop, number_of_sales)
   pet_shop[:admin][:pets_sold] += number_of_sales
@@ -25,11 +29,17 @@ end
 def pets_by_breed(pet_shop, pet_breed)
   pet_breed_count = []
   for pet in pet_shop[:pets]
-    p pet
     if pet[:breed] == pet_breed
       pet_breed_count.push(pet)
     end
   end
   return pet_breed_count
+end
 
+def find_pet_by_name(pet_shop, pet_name)
+  for pet in pet_shop[:pets]
+    if pet[:name] == pet_name
+      return pet
+    end 
+  end
 end
